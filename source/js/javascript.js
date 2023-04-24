4b4cd554c8834da636a56386dfe013f6ad5e837e
Alexander <web-wild-fox@yandex.ru>
2023-04-24 23:14:13 +0400

    Добавляет адаптивное меню + меню без js, иконки, интерактивную карту.

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -1,10 +1,16 @@
 // Получение элементов страницы
-const toggleMenu = document.getElementsByClassName('toggle-menu') [0]
-const mainNavList = document.getElementsByClassName('main-nav__list') [0]
-const toggleMenuLineTop = document.getElementsByClassName('toggle-menu__line-top') [0]
-const toggleMenuLineBottom = document.getElementsByClassName('toggle-menu__line-bottom') [0]
-const toggleMenuLineCenterFirst = document.getElementsByClassName('toggle-menu__line-centre-first') [0]
-const toggleMenuLineCenterSecond = document.getElementsByClassName('toggle-menu__line-centre-second') [0]
+const mainHeader = document.getElementsByClassName('main-header') [0];
+const mainNav = document.getElementsByClassName('main-nav') [0];
+const mainNavList = document.getElementsByClassName('main-nav__list') [0];
+const toggleMenu = document.getElementsByClassName('toggle-menu') [0];
+const toggleMenuLineTop = document.getElementsByClassName('toggle-menu__line-top') [0];
+const toggleMenuLineBottom = document.getElementsByClassName('toggle-menu__line-bottom') [0];
+const toggleMenuLineCenterFirst = document.getElementsByClassName('toggle-menu__line-centre-first') [0];
+const toggleMenuLineCenterSecond = document.getElementsByClassName('toggle-menu__line-centre-second') [0];
+
+// Если js работает, то удаляем класс --nojs
+mainNav.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-header--nojs');
 
 // Добавляет обработчик на событие "клик" для меню
 toggleMenu.addEventListener('click', function () {
@@ -16,11 +22,10 @@ toggleMenu.addEventListener('click', function () {
 });
 
 // Получение элементов страницы
-const mainHeader = document.getElementsByClassName('main-header') [0]
-const mainNavLinkHome = mainNavList.getElementsByClassName('main-nav__link') [0]
-const mainNavLinkCatalog = mainNavList.getElementsByClassName('main-nav__link') [1]
-const mainNavLinkProgram = mainNavList.getElementsByClassName('main-nav__link') [2]
-const mainPage = document.getElementsByTagName('main') [0]
+const mainNavLinkHome = mainNavList.getElementsByClassName('main-nav__link') [0];
+const mainNavLinkCatalog = mainNavList.getElementsByClassName('main-nav__link') [1];
+const mainNavLinkProgram = mainNavList.getElementsByClassName('main-nav__link') [2];
+const mainPage = document.getElementsByTagName('main') [0];
 
 // Добавляет обработчик на событие "скролл" для меню
 document.addEventListener('scroll', function() {
@@ -35,4 +40,4 @@ document.addEventListener('scroll', function() {
     mainNavLinkCatalog.classList.remove('main-nav__link--desktop-only')
     mainNavLinkProgram.classList.remove('main-nav__link--desktop-only')
   }
-})
+});
